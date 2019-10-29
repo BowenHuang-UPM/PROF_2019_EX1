4ac4e5a8cb83e7f3774b63793ebc5d3f5a63889d
Bowen.Huang <huangbowen203@gmail.com>
2019-10-29 16:22:03 +0100

    Test

--- src/main/java/es/upm/grise/profundizacion2019/ex1/ClaseExamen.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex1/ClaseExamen.java
@@ -5,18 +5,21 @@ public class ClaseExamen {
 	public float metodoAProbar(int a, int b) {
 		
 		float result = 0;
-		
+		//nodo1
 		while( a > b) {
+			//nodo2
 			a--;
 		}
-		
+		//nodo 3
 		if (a <= b) {
+			//nodo 4
 			result = 0;
 		}
 		else {
+			//nodo 5
 			result = b;
 		}
-		
+		//nodo 6
 		return result;
 	}
 
--- src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
+++ src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
@@ -4,11 +4,21 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import junit.framework.TestCase;
+
 public class ClaseExamenTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		ClaseExamen cExamen = new ClaseExamen();
+		float result1 = cExamen.metodoAProbar(2, 1);
+		TestCase.assertEquals(0f, result1);
+		
+		float result2 = cExamen.metodoAProbar(1, 2);
+		TestCase.assertEquals(0f, result2);
+		
+		float result3 = cExamen.metodoAProbar(0, 0);
+		TestCase.assertEquals(0f, result3);
 	}
 
 }
